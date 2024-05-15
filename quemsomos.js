211a3038ac6b0c7edeb55f934f7c1c77b3555439
Marcelo_Batista <145167596+MarceloBatistazul@users.noreply.github.com>
2024-05-15 01:55:22 -0300

    Add files via upload

---
+++ quemsomos.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.visibilidade-card', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:1500
+ });
+ 
---
+++ script.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.visibilidade-1', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:3000
+ });
+ sr.reveal('.visibilidade-2', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:3000
+ });
+ sr.reveal('.visibilidade-3', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:3000
+ });
+ sr.reveal('.visibilidade-4', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:3000
+ });
+ sr.reveal('.visibilidade-5', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:3000
+ });
+ sr.reveal('.visibilidade-6', { 
+    rotate: {x:0, y:80, z: 0},
+    duration:3000
+ });
